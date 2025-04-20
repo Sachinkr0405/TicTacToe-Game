1c04911e50e35605825edf9bb460a4f95af3d487
Sachin Kumar <skkumar7808051@gmail.com>
2025-04-20 22:57:21 +0530

    main function added

--- main.java
+++ main.java
@@ -92,5 +92,77 @@ class JavaProject {
         System.out.println("|---|---|---|");
     }
 
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        board = new String[9];
+        turn = "X";
+        String winner = null;
+
+        for (int a = 0; a < 9; a++) {
+            board[a] = String.valueOf(a + 1);
+        }
+
+        System.out.println("Welcome to 3x3 Tic Tac Toe.");
+        printBoard();
+
+        System.out.println(
+                "X will play first. Enter a slot number to place X in:");
+
+        while (winner == null) {
+            int numInput;
+
+
+            try {
+                numInput = in.nextInt();
+                if (!(numInput > 0 && numInput <= 9)) {
+                    System.out.println(
+                            "Invalid input; re-enter slot number:");
+                    continue;
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println(
+                        "Invalid input; re-enter slot number:");
+                continue;
+            }
+
+
+            if (board[numInput - 1].equals(
+                    String.valueOf(numInput))) {
+                board[numInput - 1] = turn;
+
+                if (turn.equals("X")) {
+                    turn = "O";
+                }
+                else {
+                    turn = "X";
+                }
+
+                printBoard();
+                winner = checkWinner();
+            }
+            else {
+                System.out.println(
+                        "Slot already taken; re-enter slot number:");
+            }
+        }
+
+
+        if (winner.equalsIgnoreCase("draw")) {
+            System.out.println(
+                    "It's a draw! Thanks for playing.");
+        }
+
+
+        else {
+            System.out.println(
+                    "Congratulations! " + winner
+                            + "'s have won! Thanks for playing.");
+        }
+        in.close();
+    }
+
+
 }
 
